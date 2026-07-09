cd2edaecd7863f6695c22a2b07b560a0c6152271
splidje <jonathan.hearn@ajptechnical.com>
2026-07-09 08:52:32 +0100

    Revert "[opencue_requires_gpu] (it rhymes) Added checkbox to write node `requires_gpu` as a shortcut to setting the OpenCue publish attribute, so "Render On Farm" button picks it up."
    This reverts commit b184c643961e3a0361835268b66aa5b440460c72.

--- client/ayon_nuke/api/pipeline.py
+++ client/ayon_nuke/api/pipeline.py
@@ -59,7 +59,6 @@ from .lib import (
     dirmap_file_name_filter,
     add_scripts_menu,
     add_scripts_gizmo,
-    group_node_knob_changed,
     get_node_data,
     set_node_data,
     MENU_LABEL,
@@ -199,9 +198,6 @@ def add_nuke_callbacks(project_settings: dict = None):
         log.info("Added Nuke's dir-mapping callback ...")
         # Add dirmap for file paths.
         nuke.addFilenameFilter(dirmap_file_name_filter)
-
-    # add callback for opencue requires gpu knob
-    nuke.addKnobChanged(group_node_knob_changed, nodeClass="Group")
 
     log.info("Added Nuke callbacks ...")
 
